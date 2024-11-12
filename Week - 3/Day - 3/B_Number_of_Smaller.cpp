2ca0c6f790c2f71e07011e1e5aaa8f1dc85e65e5
Yeasin Arafat <122679771+arafat69691@users.noreply.github.com>
2024-11-13 00:24:30 +0600

    Add files via upload

---
+++ Week - 3/Day - 3/B_Number_of_Smaller.cpp
@@ -0,0 +1,38 @@
+/*
+Author: Yeasin Arafat
+Source:
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+     
+     int a,b;
+     cin>>a>>b;
+     vector<int>p(a),q(b);
+     for(int i = 0 ; i<a ; i++)
+     {
+        cin>>p[i];
+     }
+     for(int i = 0 ; i<b ; i++)
+     {
+        cin>>q[i];
+     }
+    int l = 0 ,r=0,cnt=0;
+     while(r<b)
+     {
+        if(l<a && p[l]<q[r])
+        {
+            cnt++;
+            l++;
+        }
+        else 
+        {
+            cout<<cnt<<" ";
+            r++;
+        }
+     }
+    return 0;
+}
