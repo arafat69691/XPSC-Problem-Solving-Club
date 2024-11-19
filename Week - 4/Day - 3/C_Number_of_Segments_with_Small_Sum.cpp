a99e6aeb0d0364fa96030330e41ace7a9d88fba2
Yeasin Arafat <122679771+arafat69691@users.noreply.github.com>
2024-11-19 23:25:17 +0600

    Add files via upload

---
+++ Week - 4/Day - 3/C_Number_of_Segments_with_Small_Sum.cpp
@@ -0,0 +1,16 @@
+/*
+Author: Yeasin Arafat
+Source:
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+     
+     int n,k;
+     cin>>n>>k;
+     
+    return 0;
+}
